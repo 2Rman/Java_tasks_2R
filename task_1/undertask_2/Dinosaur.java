a56d643a2e657415e6189cf73e45ffa34e3968bd
Tworman <i_ray90@mail.ru>
2020-06-23 18:32:36 +0300

    Done

---
+++ task_1/undertask_2/Dinosaur.java
@@ -0,0 +1,15 @@
+package undertask_2;
+
+import java.util.Scanner;
+
+public class Dinosaur {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Сколько весит динозавр?");
+        int dino_kg = in.nextInt();
+        double dino_t = (double)dino_kg/1000;
+        long dino_gr = (dino_kg * 1000);
+        long dino_mg = (dino_gr * 1000);
+        System.out.println("Значит он весит:\n" + dino_t + " тонн, \n" + dino_gr + " грамм, \n" + dino_mg + " миллиграмм!");
+    }
+}
