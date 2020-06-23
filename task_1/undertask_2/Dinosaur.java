bc106f59cfed8f465ee5e9e79ace08af97b8f584
Jahimees <36009821+Jahimees@users.noreply.github.com>
2020-06-23 18:29:52 +0200

    Refactoring 2
    Недочеты по оформлению

--- task_1/undertask_2/Dinosaur.java
+++ task_1/undertask_2/Dinosaur.java
@@ -6,10 +6,13 @@ public class Dinosaur {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Сколько весит динозавр?");
-        int dino_kg = in.nextInt();
-        double dino_t = (double)dino_kg/1000;
-        long dino_gr = (dino_kg * 1000);
-        long dino_mg = (dino_gr * 1000);
+        //Отделил логический блок
+        
+        int dino_kg = in.nextInt(); //Переменные в java называются в camelCase-стиле, то есть dinoKg
+        double dino_t = (double) dino_kg / 1000; //Пробелы ;) + название переменной
+        long dino_gr = (dino_kg * 1000); // название переменной
+        long dino_mg = (dino_gr * 1000); //название переменной
+        
         System.out.println("Значит он весит:\n" + dino_t + " тонн, \n" + dino_gr + " грамм, \n" + dino_mg + " миллиграмм!");
     }
 }
