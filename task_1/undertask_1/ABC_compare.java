e6662b8ff30e97ccf379f811465126e0acfadb24
Tworman <i_ray90@mail.ru>
2020-06-23 17:15:27 +0300

    Done

---
+++ task_1/undertask_1/ABC_compare.java
@@ -0,0 +1,19 @@
+package undertask_1;
+
+public class ABC_compare {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 4;
+        b = 5;
+        c = 6;
+        if (a!=b) {
+            System.out.println("a не равно b");
+        } else System.out.println("a равно b");
+        if (b!=c) {
+            System.out.println("b не равно c");
+        } else System.out.println("b равно c");
+        if (c!=a) {
+            System.out.println("c не равно a");
+        } else System.out.println("c равно a");
+    }
+}
