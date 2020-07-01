c34592575137c1a456debbfd6afa963d4c92b03e
TheFearka <JerryMous1901@gmail.com>
2020-07-01 19:54:50 +0300

    Рефакторинг четвертой задачи

--- task_1/undertask_4/SequenceComparator.java
+++ task_1/undertask_4/SequenceComparator.java
@@ -1,9 +1,17 @@
 package undertask_4;
-// Число должно образовывать возрастающую последовательность
+
 
 import java.util.Scanner;
 
-public class num_compare {
+/**
+ * Число должно образовывать возрастающую последовательность
+ */
+//Грубое замечание по поводу именования.
+//Название классов:
+// 1) с большой буквы.
+// 2) Без нижних подчеркиваний. Если два слова, то стиль CamelCase
+// 3) Наименование должно быть существительным
+public class SequenceComparator {
     public static void main(String[] args) {
         System.out.println("---Проверяем, что цифры четырёхзначного числа образуют возрастающую или убывающую последовательность ---");
         System.out.println("Введите четырехзначное число...");
@@ -11,11 +19,15 @@ public class num_compare {
         Scanner numScan = new Scanner(System.in);
         String num = numScan.nextLine();
 
+        //Желательно переделать через массив int[] и сделать через цикл. Помним, что у String, есть поле length,
+        //которое возвращает длину строки
+        //Подумай сам, исправлять не буду
         int fir = num.charAt(0);
         int sec = num.charAt(1);
         int thr = num.charAt(2);
         int fou = num.charAt(3);
 
+        //Остальное в норме. Плюсик за то, что начал разбивать всё по блокам :)
         if ((fir > sec) && (fir > thr) && (fir > fou)) {
             if ((sec > thr) && (sec > fou)) {
                 if (thr > fou) {
