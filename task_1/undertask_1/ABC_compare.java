4c5d0573e7838edbd2dbba58dbad4cab3439bffa
TheFearka <JerryMous1901@gmail.com>
2020-07-01 19:36:36 +0300

    Рефакторинг первой задачи

--- task_1/undertask_1/ABC_compare.java
+++ task_1/undertask_1/ABC_compare.java
@@ -1,25 +1,35 @@
 package undertask_1;
 
-import org.w3c.dom.ls.LSInput;
-import javax.swing.*;
-import java.sql.SQLOutput;
+//Убрал лишние импорты
 import java.util.Scanner;
 
 public class ABC_compare {
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
         System.out.println("Введите числа a b c для сравнения");
+        // Разделил на блоки для читаемости
+
         int a = in.nextInt();
         int b = in.nextInt();
         int c = in.nextInt();
+
+        // Добавил скобки к else
         if (a!=b) {
             System.out.println("a не равно b");
-        } else System.out.println("a равно b");
+        } else {
+            System.out.println("a равно b");
+        }
+
         if (b!=c) {
             System.out.println("b не равно c");
-        } else System.out.println("b равно c");
+        } else {
+            System.out.println("b равно c");
+        }
+
         if (c!=a) {
             System.out.println("c не равно a");
-        } else System.out.println("c равно a");
+        } else {
+            System.out.println("c равно a");
+        }
     }
 }
